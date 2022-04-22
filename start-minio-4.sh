ecea96f0bcebff17cd2f569b29db61b4104d6f12
djl <dujianglin17@mails.ucas.ac.cn>
2022-04-22 14:53:33 +0800

    commit

--- start-minio-4.sh
+++ start-minio-4.sh
@@ -1,6 +1,6 @@
 export MINIO_ACCESS_KEY=any
 export MINIO_SECRET_KEY=anyanyany
-./minio server --address :9726 \
+../minio server --address :9726 \
                 http://10.10.1.20:9726/mnt/disk1/minio/data \
                 http://10.10.1.32:9726/mnt/disk1/minio/data \
                 http://10.10.1.36:9726/mnt/disk1/minio/data \
